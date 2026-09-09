82b7e9ca041bfe8131f7fdce81f6a9913d51b57e
HEBA <heba15112006@gmail.com>
2026-09-09 23:29:45 +0300

    Initialization Deposit Method

---
+++ InitializationDepositMethod.cs
@@ -0,0 +1,173 @@
+using static Bank_System_V_01.Program;
+namespace Bank_System_V_01
+{
+    internal class Program
+    {
+        public struct Account
+        {
+            public string Name;
+            public string Number;
+            public double Balance;
+
+        }
+        static List<Account> accounts = new List<Account>();
+
+        public static void showChoices()
+        {
+            Console.WriteLine("========== BANKING SYSTEM ==========\n\n");
+            Console.WriteLine(" 1. Create Account ");
+            Console.WriteLine(" 2. Deposit");
+            Console.WriteLine(" 3. Withdraw");
+            Console.WriteLine(" 4. Transfer Money");
+            Console.WriteLine(" 5. Check Balance");
+            Console.WriteLine(" 6. Account Information");
+            Console.WriteLine(" 7. Transaction History");
+            Console.WriteLine(" 8. Search Account");
+            Console.WriteLine(" 9. Exit ");
+            Console.WriteLine("\n");
+
+        }
+
+        public static void CreateAccount()
+        {
+            Account account = new Account();
+
+            Console.WriteLine("========== Create Account ==========\n");
+            Console.WriteLine("Enter your name: ");
+            string acccountName = Console.ReadLine();
+            account.Name = acccountName;
+
+            Console.WriteLine("Enter account number: ");
+            string accountNumber = Console.ReadLine();
+            account.Number = accountNumber;
+
+
+            Console.WriteLine("Enter initial balance: ");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            account.Balance = balance;
+
+            bool found = false;
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (accounts[i].Number == accountNumber)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+                Console.WriteLine("Account Number is Exists..");
+            else
+            {
+                accounts.Add(account);
+                Console.WriteLine("Account created successfully!");
+            }
+
+            Console.WriteLine("\r\n\r\n====================================");
+
+        }
+        public static void Deposit()
+        {
+            Console.WriteLine("========== Deposit ==========\r\n");
+            int m = -1;
+            Console.WriteLine("Account Number  : ");
+            string accountNumber = Console.ReadLine();
+            bool found = false;
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (accounts[i].Number == accountNumber)
+                {
+                    found = true;
+                    m = i;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                Console.WriteLine("Enter amount:");
+                double amount = Convert.ToDouble(Console.ReadLine());
+                Account account = accounts[m];
+                account.Balance += amount;
+                accounts[m] = account;
+                Console.WriteLine($"New balance: {accounts[m].Balance}");
+            }
+            else
+                Console.WriteLine("Account is not Exists ...");
+
+            Console.WriteLine("================================");
+        }
+        public static void Withdraw(ref Account Account)
+        {
+            Console.WriteLine("3");
+        }
+        public static void TransferMoney(ref Account Account)
+        {
+            Console.WriteLine("4");
+        }
+        public static void CheckBalance(ref Account Account)
+        {
+            Console.WriteLine("5");
+        }
+        public static void Accountrmation(ref Account Account)
+        {
+            Console.WriteLine("6");
+        }
+        public static void TransactionHistory(ref Account Account)
+        {
+            Console.WriteLine("7");
+        }
+        public static void SearchAccount(ref Account Account)
+        {
+            Console.WriteLine("8");
+        }
+        public static void Exit()
+        {
+            Console.WriteLine("Thank you for using our Bank System!\r\n" +
+                "Goodbye ");
+            Console.WriteLine("\r\r\n====================================");
+        }
+
+        public static void ChoiseOpr(ref Account account, ref int n)
+        {
+            if (n == 1)
+                CreateAccount();
+            else if (n == 2)
+                Deposit();
+            else if (n == 3)
+                Withdraw(ref account);
+            else if (n == 4)
+                TransferMoney(ref account);
+            else if (n == 5)
+                CheckBalance(ref account);
+            else if (n == 6)
+                Accountrmation(ref account);
+            else if (n == 7)
+                TransactionHistory(ref account);
+            else if (n == 8)
+                SearchAccount(ref account);
+            else if (n == 9)
+                Exit();
+        }
+        public static void StartProject()
+        {
+            Account account = new Account(); // initialize before use
+            int oprNum;
+            do
+            {
+                showChoices();
+                Console.Write("Enter Choice [1-9] : ");
+                oprNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("\r\n\r\n====================================\n");
+                ChoiseOpr(ref account, ref oprNum);
+
+            } while (oprNum != 9);
+
+        }
+        static void Main(string[] args)
+        {
+            StartProject();
+        }
+    }
+}
